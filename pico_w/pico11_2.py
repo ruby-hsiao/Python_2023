95aa8d472349ee0aa69fcfa9363bc12add4be1e7
ruby-hsiao <151415126+ruby-hsiao@users.noreply.github.com>
2024-01-06 14:58:25 +0800

    Pico alert

--- pico_w/pico11_2.py
+++ pico_w/pico11_2.py
@@ -1,6 +1,6 @@
 import network
 import time
-from machine import WDT, Timer
+from machine import WDT, Timer, ADC
 
 #連線等待10秒
 def connect():
@@ -33,12 +33,27 @@ def connect():
         print("成功連線")
         print(nic.ifconfig())
 
-def doTimer(t:Timer):
-    print("Reconnect iphone wifi...")
-    t.deinit()
 
-connect()
-#若連線10秒不成功，可以用Timer來接續處理
-tim = Timer()
-tim.init(period=1000, callback=doTimer)
+def alert():
+    print('要爆炸了!')
+    
+def doTimer(t:Timer):
+    global start
+    sensor = ADC(4)    
+    vol = sensor.read_u16() * (3.3/65535)
+    temperature = 27 - (vol-0.706) / 0.001721
+    print(f'溫度:{temperature}')    
+    delta = time.ticks_diff(time.ticks_ms(), start)
+    print(delta)
+    #溫度超過24度,並且發送alert()的時間已經大於60秒
+    if temperature >= 24 and delta >= 60 * 1000:        
+        alert()
+        start = time.ticks_ms()#重新設定計時的時間
+        
+
+connect()       
+
+start = time.ticks_ms() - 60 * 1000 #應用程式啟動時,計時時間,先減60秒    
+time1 = Timer()
+time1.init(period=1000,callback=doTimer)
 
