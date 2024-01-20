ccfbec4dc7eff144515cab4a5b7f153b7cc83b92
ruby-hsiao <151415126+ruby-hsiao@users.noreply.github.com>
2024-01-20 14:54:24 +0800

    抽取成function

--- pico_w/button.py
+++ pico_w/button.py
@@ -3,39 +3,25 @@ import time
 
 led = Pin(15, Pin.OUT)
 #button = Pin(14, Pin.IN, Pin.PULL_DOWN)
-button = Pin(14, Pin.IN, Pin.PULL_DOWN)
+button = Pin(14, Pin.PULL_DOWN)
 is_press = False
 led_status = False
 
-while True:
-    #print(button.value())
-    #time.sleep_ms(500)
-  
-    #if button.value():
-    #    led.value(1)
-    #else:
-    #    led.value(0)
-    
-    #解決彈跳電容的錢
-    #switch button
-    if button.value():
+def btn_detect(btn):
+    global is_press, led_status
+    if btn.value():
         #發生彈跳的解法
         time.sleep_ms(50)
-        if button.value():
+        if btn.value():
             is_press = True
     elif is_press:
         time.sleep_ms(50)
-        if button.value() == False:
+        if btn.value() == False:
             print('release')
             led_status = not led_status
             print(f'led_status={led_status}')
             led.value(led_status)
             is_press = False
- 
-    #time.sleep_ms(200)
-        
-    
-'''
-        led.toggle()
-        time.sleep(0.5)
-'''        
+            
+while True:
+    btn_detect(button)
