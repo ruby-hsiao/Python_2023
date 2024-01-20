de9b56637bb0a8cf4fbd03520fc4d0d451c30cfb
ruby-hsiao <151415126+ruby-hsiao@users.noreply.github.com>
2024-01-20 11:47:02 +0800

    click btn then light up

--- pico_w/button.py
+++ pico_w/button.py
@@ -6,10 +6,14 @@ led = Pin(15, Pin.OUT)
 button = Pin(14, Pin.PULL_DOWN)
 
 while True:
-    print(button.value())
-    time.sleep_ms(500)
-'''    
+    #print(button.value())
+    #time.sleep_ms(500)
+  
     if button.value():
+        led.value(1)
+    else:
+        led.value(0)
+'''
         led.toggle()
         time.sleep(0.5)
 '''        
