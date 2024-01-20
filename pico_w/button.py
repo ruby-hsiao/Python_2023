a77970640ed4f22a09def7a8158272b9ae926d69
ruby-hsiao <151415126+ruby-hsiao@users.noreply.github.com>
2024-01-20 13:59:43 +0800

    button on/off ctrl LED

--- pico_w/button.py
+++ pico_w/button.py
@@ -3,16 +3,33 @@ import time
 
 led = Pin(15, Pin.OUT)
 #button = Pin(14, Pin.IN, Pin.PULL_DOWN)
-button = Pin(14, Pin.PULL_DOWN)
+button = Pin(14, Pin.IN, Pin.PULL_DOWN)
+is_press = False
+led_status = False
 
 while True:
     #print(button.value())
     #time.sleep_ms(500)
   
+    #if button.value():
+    #    led.value(1)
+    #else:
+    #    led.value(0)
+    
+    #解決彈跳電容的錢
+    #switch button
     if button.value():
-        led.value(1)
-    else:
-        led.value(0)
+        is_press = True
+    elif is_press:
+        print('release')
+        led_status = not led_status
+        print(f'led_status={led_status}')
+        led.value(led_status)
+        is_press = False
+ 
+    time.sleep_ms(200)
+        
+    
 '''
         led.toggle()
         time.sleep(0.5)
