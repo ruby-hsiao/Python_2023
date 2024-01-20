423bca94c3847bc18c74442eef4ebf1956e4a081
ruby-hsiao <151415126+ruby-hsiao@users.noreply.github.com>
2024-01-20 16:23:04 +0800

    Pico btn ctrl Blynk App

--- pico_w/button.py
+++ pico_w/button.py
@@ -1,11 +1,14 @@
 from machine import Pin
 import time
+import urequests as requests
+from tools import connect,reconnect
 
 led = Pin(15, Pin.OUT)
 #button = Pin(14, Pin.IN, Pin.PULL_DOWN)
 button = Pin(14, Pin.PULL_DOWN)
 is_press = False
 led_status = False
+
 
 def btn_detect(btn):
     global is_press, led_status
@@ -23,5 +26,22 @@ def btn_detect(btn):
             led.value(led_status)
             is_press = False
             
+            try:
+                if led_status == True:
+                    get_url = 'https://blynk.cloud/external/api/update?token=Mcr1vSCkqhfcoFpDr5aMN92mDNWKD0EC&v0=1'
+                else:
+                    get_url = 'https://blynk.cloud/external/api/update?token=Mcr1vSCkqhfcoFpDr5aMN92mDNWKD0EC&v0=0'  
+                response = requests.get(get_url)
+            except:
+                reconnect()
+            else:
+                if response.status_code == 200:
+                    print("傳送成功")
+                else:
+                    print("server有錯誤訊息")
+                    print(f'status_code:{response.status_code}')
+                response.close()
+
+connect()
 while True:
     btn_detect(button)
