6138bfdd7e842ea5b96996417d89f150889c2628
ruby-hsiao <151415126+ruby-hsiao@users.noreply.github.com>
2024-01-27 16:20:58 +0800

    3dv update via Blynk

--- pico_w/lesson14_1.py
+++ pico_w/lesson14_1.py
@@ -3,22 +3,28 @@ import time
 from tools import connect, reconnect
 import urequests as requests
 
-
+led_status = 0
 
 def fun10(t:Timer | None = None):    
     print('5秒')
-    led.toggle() #turn on/off led
+    #led.toggle() #turn on/off led
 
 def fun10ms(t:Timer):
     print(f'light:{light.read_u16()}')
     print(f'vr:{vr.read_u16()}')
 
 def updateDV(t:Timer):
+    global led_status
     light_value = light.read_u16()
     vr_value = vr.read_u16()
-    url = f'https://blynk.cloud/external/api/batch/update?token=Mcr1vSCkqhfcoFpDr5aMN92mDNWKD0EC&v1={vr_value}&v2={light_value}'    
+        
     try:
-        led.value(1)
+        if led_status == 0:
+            led_status = 1
+        else:
+            led_status = 0
+            
+        url = f'https://blynk.cloud/external/api/batch/update?token=Mcr1vSCkqhfcoFpDr5aMN92mDNWKD0EC&v1={vr_value}&v2={light_value}&v0={led_status}'
         response = requests.get(url)
     except:
         #連線失敗
@@ -27,12 +33,17 @@ def updateDV(t:Timer):
         #連線成功且get回傳成功
         if response.status_code == 200:
             print('連線成功')
+            print(f'light:{light.read_u16()}')
+            print(f'vr:{vr.read_u16()}')
+            print(f'led:{led_status}')
+            led.value(led_status)
         else:
         #連線成功但server有問題
+            led_status = 0
             print("server有錯誤訊息")
             print(f'status_code:{response.status_code}')
         response.close()
-    led.value(0)
+    
     
 
 connect()
