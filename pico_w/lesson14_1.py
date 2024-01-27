dcc1c36e7a00c0e2f2a5caa64bc600c5d8410a43
ruby-hsiao <151415126+ruby-hsiao@users.noreply.github.com>
2024-01-27 14:38:18 +0800

    光敏電阻偵測光線

--- pico_w/lesson14_1.py
+++ pico_w/lesson14_1.py
@@ -4,15 +4,18 @@ import time
 def fun10(t:Timer | None = None):    
     print('5秒')
     led.toggle() #turn on/off led
-    
+
+def fun10ms(t:Timer):
+    print(light.read_u16())
+
 led = Pin(15, Pin.OUT)
 light = ADC(Pin(28))
 timer10 = Timer(period=5000, mode=Timer.PERIODIC, callback=fun10)
+timer10ms = Timer(period=500, mode=Timer.PERIODIC, callback=fun10ms)
 fun10() #trigger function first
 
 
-while True:
-    print(light.read_u16())
-    time.sleep_ms(500)
+
+
 
 
