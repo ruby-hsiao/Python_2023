bf4fff678a6a8f54ffbb1684efdbd44947963401
ruby-hsiao <151415126+ruby-hsiao@users.noreply.github.com>
2024-01-27 14:43:12 +0800

    偵測可變電阻

--- pico_w/lesson14_1.py
+++ pico_w/lesson14_1.py
@@ -6,10 +6,12 @@ def fun10(t:Timer | None = None):
     led.toggle() #turn on/off led
 
 def fun10ms(t:Timer):
-    print(light.read_u16())
+    print(f'light:{light.read_u16()}')
+    print(f'vr:{vr.read_u16()}')
 
 led = Pin(15, Pin.OUT)
-light = ADC(Pin(28))
+light = ADC(Pin(28)) #光敏電阻接Pin28
+vr = ADC(Pin(27)) #可變電阻接Pin27
 timer10 = Timer(period=5000, mode=Timer.PERIODIC, callback=fun10)
 timer10ms = Timer(period=500, mode=Timer.PERIODIC, callback=fun10ms)
 fun10() #trigger function first
