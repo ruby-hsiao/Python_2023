20570086278b7ddecc1e585c2318950d1006755b
ruby-hsiao <151415126+ruby-hsiao@users.noreply.github.com>
2024-01-27 14:27:05 +0800

    偵測ADC光線變化 w/ sleep_ms

--- pico_w/lesson14_1.py
+++ pico_w/lesson14_1.py
@@ -1,6 +1,18 @@
-from machine import Timer, Pin
+from machine import Timer, Pin, ADC
+import time
 
-def fun10(t:Timer):
-    print('10秒')
+def fun10(t:Timer | None = None):    
+    print('5秒')
+    led.toggle() #turn on/off led
     
-timer10 = Timer(period=10000, mode=Timer.PERIODIC, callback=fun10)
+led = Pin(15, Pin.OUT)
+light = ADC(Pin(28))
+timer10 = Timer(period=5000, mode=Timer.PERIODIC, callback=fun10)
+fun10() #trigger function first
+
+
+while True:
+    print(light.read_u16())
+    time.sleep_ms(500)
+
+
